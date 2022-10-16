43f9c5085c0c2816b2e2b4501d799e9062eb3f52
castl-star <5354artem@gmail.com>
2022-10-16 17:48:33 +1000

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('test')
+function calc(operation, num1, num2) {
+    switch (operation) {
+        case 'add':
+            return num1 + num2
+        case 'multi':
+            return num1 * num2
+        case 'subtract':
+            return num1 - num2
+
+        default:
+            alert('Вы ввели что-то не то, попробуйте заново!')
+    }
+}
+console.log(calc('add', 2, 2)) //4
+console.log(calc('multi', 3, 10)) //30
+console.log(calc('subtract', 10, -5)) //15
